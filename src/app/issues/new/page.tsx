81beb2b562e2a367bde4d8382e3842cff45c4ccb
himanshu <himanshusingh3179@gmail.com>
2023-12-02 16:13:26 +0530

    issue status badge

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -9,6 +9,8 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
+import { ErrorMessage } from '@/components/ErrorMessage';
+import { Spinner } from '@/components/Spinner';
 
 
 type FormData = z.infer<typeof createIssueSchema>
@@ -19,6 +21,20 @@ export default function NewIssue() {
     });
     const router = useRouter()
     const [error, setError] = useState('')
+    const [submitting, setSubmitting] = useState(false);
+
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true)
+            await axios.post('/api/issues', data);
+            router.push("/issues")
+        } catch (error) {
+            setSubmitting(false)
+            console.log(error)
+            setError('An unexpected error occurred')
+        }
+    })
+
     return (
         <div className='max-w-xl'>
             {
@@ -28,23 +44,11 @@ export default function NewIssue() {
                     </Callout.Text>
                 </Callout.Root>)
             }
-            <form className=' space-y-3' onSubmit={handleSubmit(async (data) => {
-
-                try {
-                    await axios.post('/api/issues', data);
-                    router.push("/issues")
-                } catch (error) {
-                    console.log(error)
-                    setError('An unexpected error occurred')
-
-                }
-            })}>
+            <form className=' space-y-3' onSubmit={onSubmit}>
                 <TextField.Root>
                     <TextField.Input placeholder="Title"  {...register("title")} />
                 </TextField.Root>
-                {errors.title && (<Text as='p' color='red'>
-                    {errors.title.message}
-                </Text>)}
+                <ErrorMessage >{errors.title?.message}</ErrorMessage>
                 <Controller
                     name='description'
                     control={control}
@@ -52,14 +56,11 @@ export default function NewIssue() {
                         <SimpleMDE placeholder='description' {...field} />
                     )}
                 />
-                {
-                    errors.description && (
-                        <Text as='p' color='red'>
-                            {errors.description.message}
-                        </Text>
-                    )
-                }
-                <Button>Submit New Issue</Button>
+                <ErrorMessage >{errors.description?.message}</ErrorMessage>
+                <Button disabled={submitting}>
+                    Submit New Issue
+                    {submitting && <Spinner />}
+                </Button>
             </form>
         </div>
     )
